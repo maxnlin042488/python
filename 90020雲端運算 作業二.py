cdb813e859f19651d0a91c9c5104ccf994d60533
maxnlin042488 <70868225+maxnlin042488@users.noreply.github.com>
2020-09-20 23:12:50 +0800

    Add files via upload
    90020

---
+++ 90020雲端運算 作業二.py
@@ -0,0 +1,32 @@
+#3. 請練習使用索引值的方式將'I love you'裡的三個字取出來
+# string ='I Love You'
+# string[0:4]
+# print(string[0:4])
+###################################
+#4. 承上題，將取出的I love you反轉成uoy evol I。
+# string ='I Love You'
+# string[::-1]
+# print(string[::-1])
+###################################
+# #明寫情書給女朋友abey，想寫I love you so so ... (100次) much。請用Python來協助他。
+# a = 'I Love You'
+# b = ' So'
+# c = ' much'
+# print(a+ b*100+c)
+####################################
+# a = 'I Love You'
+# b = ' So'
+# c = ' much' 
+# import random #random 是隨機數模組套件
+
+# x = random.randint(1, 100)#設定隨機數X 
+# print(a + b*x + c)
+
+
+
+
+
+
+
+
+
