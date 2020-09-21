674c1abd862d0677e32826d1a1c4216e26626035
maxnlin042488 <70868225+maxnlin042488@users.noreply.github.com>
2020-09-21 09:11:15 +0800

    Add files via upload

---
+++ 90020雲端運算作業二 (2).py
@@ -0,0 +1,89 @@
+# string ='I Love You'
+# a = string[0]
+# b = string[2:6]
+# c = string[7:10]
+# print(a,b,c)
+
+# ###################################
+
+#4. 承上題，將取出的I love you反轉成uoy evol I。
+
+# string ='I Love You'
+
+# string[::-1]
+
+# print(string[::-1])
+
+###################################
+
+# #明寫情書給女朋友abey，想寫I love you so so ... (100次) much。請用Python來協助他。
+
+# a = 'I Love You'
+
+# b = ' So'
+
+# c = 'much'
+
+# print(a,b*100,c)
+
+####################################
+
+# a = 'I Love You'
+
+# b = ' So'
+
+# c = 'much' 
+
+# import random #random 是隨機數模組套件
+
+# x = random.randint(1, 100)#設定隨機數X 
+ 
+# print(a,b*x,c)
+
+# d = b*x
+
+# sub = 'So'
+
+# print('So 的次數',d.count(sub))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
