296935a7a51a0ab1a63ed4de1eb53600c5128915
maxnlin042488 <70868225+maxnlin042488@users.noreply.github.com>
2020-10-12 08:56:32 +0800

    Add files via upload

---
+++ 雲端運算作業五.py
@@ -0,0 +1,30 @@
+# 3. 請從兩個串列裡，輸出對應較大的值，例如，串列l1, l2分別為：
+# l1 = [1,2,3,4,5]
+# l2 = [6,5,4,3,2]
+# 執行結果：
+# [6, 5, 4, 4, 5]
+# list1 = [1,2,3,4,5]
+# list2 = [6,5,4,3,2]
+# for i in range(0,5):
+#     if list1[i]>list2[i]:
+#         print(list1[i],end=' ')
+#     else:
+#         print(list2[i],end=' ')
+
+# 4. 假設有三個串列
+# l1 = [1,2,3,4,5]
+# l2 = [6,5,4,3,2]
+# l3 = [False,False,True,False,True]
+# 當l3為True選串列l1的元素，如果False時選串列l2的元素。
+# 執行結果：
+# [6, 5, 3, 3, 5]
+        
+# list1 = [1,2,3,4,5]
+# list2 = [6,5,4,3,2]
+# list3 = [False,False,True,False,True]       
+# for i in range(0,5):
+#     if list3[i]:
+#        print(list1[i],end=' ')
+#     else:
+#        print(list2[i],end=' ')        
+        
